f4797e7dfe3d9d5590d66515540a60e1f54ddf73
Mina Ghaly <minanabil@msn.com>
2022-10-19 19:24:26 +1100

    basic components added, a main component added as well

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'Home') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume')
+      return <Resume />;
+  };
+
+
+  return (
+   <div>
+{About()}
+   </div>
+  );
+}
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+      Hi, my name is Mina Ghaly.
+      I started my career when I joined the bootcamp program in may 2022.
+      We studied there Javascript, html, css, express and react in depth. With this syllabus, 
+      I became very good in all these fields. In addition to that, I am 
+      planning to study php as well by the end of 2022 to have an excellent protofolio.
+      </p>
+    </div>
+  );
+}
