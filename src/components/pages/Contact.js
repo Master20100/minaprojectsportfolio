f8e16cb35eacdcad153079691636fcbf1aa01afc
Mina Ghaly <minanabil@msn.com>
2022-10-22 13:09:58 +1100

    contact me page added

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,10 +3,30 @@ import React from 'react';
 export default function Contact() {
   return (
 <section id="contact_me">
-<h2>Contact me</h2>
+  <h1>Contact</h1>
+<form>
+<label>Name:</label>
+<br></br>
+<input className='contactInput'></input>
+<br></br>
+<label>Email address:</label>
+<br></br>
+<input className='contactInput'></input>
+<br></br>
+<label>Message:</label>
+<br></br>
+<textarea rows="4" className='contactInput'></textarea><br></br>
+<input type='Submit'></input>
+</form>
+
+
+
+
+
+{/* <h2>Contact me</h2>
 <h4>Mobile: 0456770859</h4>
 <h4>Github: <a href="http://www.github.com/Master20100">www.github.com/Master20100 </a></h4>
-<h4>email: <a href = "mailto: minanabil@msn.com">minanabil@msn.com</a></h4>
+<h4>email: <a href = "mailto: minanabil@msn.com">minanabil@msn.com</a></h4> */}
 </section>
   );
 }
