7a05c3a8fb6cd1c7b3d78b347941bdf02b53e696
Mina Ghaly <minanabil@msn.com>
2022-10-22 14:01:59 +1100

    name and email validation added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ const App = ()=> <PortfolioContainer/>;
 
 export default App;
 
-// WHEN I am presented with the Contact section
-// THEN I see a contact form with fields for a name, an email address, and a message
 // WHEN I move my cursor out of one of the form fields without entering text
 // THEN I receive a notification that this field is required
 // WHEN I enter text into the email address field
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
 export default function Contact() {
+  function mouseOutEvent(e){
+  
+    switch(e.target.id) {
+      case 'contactName':
+        if(!e.target.value){alert("please enter a name")}
+        break;
+      case 'contactEmail':
+        if(!e.target.value){alert("please enter an email")}
+        else if(!/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(e.target.value)){alert('please enter a valid email')}
+        break;
+        }
+      
+    }
+
   return (
 <section id="contact_me">
   <h1>Contact</h1>
 <form>
 <label>Name:</label>
 <br></br>
-<input className='contactInput'></input>
+<input className='contactInput' id='contactName'   onMouseOut={(e) => { mouseOutEvent(e)}}></input>
 <br></br>
 <label>Email address:</label>
 <br></br>
-<input className='contactInput'></input>
+<input className='contactInput' id='contactEmail'  onMouseOut={(e) => { mouseOutEvent(e)}}></input>
 <br></br>
 <label>Message:</label>
 <br></br>
